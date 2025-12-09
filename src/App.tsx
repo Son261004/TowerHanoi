d0a63159b4e765696c34ff3748546d529e6410c0
sang <letuansang2610>
2025-12-10 00:32:13 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import HanoiTower from "./modules/hanoi_tower";
+
+const App = () => {
+  return (
+    <div className="flex flex-col gap-y-4 pt-5 items-center justify-center text-black">
+      <HanoiTower />
+    </div>
+  );
+};
+
+export default App;
